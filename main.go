472ae7d30c82c4b3e40689be0437d8e57abc7e83
Shawn Milochik <shawn@milochik.com>
2016-12-08 23:03:24 -0500

    initial

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	filename := flag.String("f", "", "log file name")
+	maxSize := flag.Int("s", 10, "max log file size in MB")
+	flag.Parse()
+	if *filename == "" {
+		fmt.Printf("filename is required\n")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	fmt.Println("vim-go")
+	fmt.Printf("filename: %q, size %d mb\n", *filename, *maxSize)
+
+	f, err := os.Create(*filename)
+	if err != nil {
+		fmt.Printf("failed to open %q: %s\n", err)
+		os.Exit(1)
+	}
+
+	remaining := *maxSize * 1024 * 1024
+	scn := bufio.NewScanner(os.Stdin)
+	for scn.Scan() {
+		if len(scn.Text()) > remaining {
+			f, err = replaceF(*filename, f)
+			if err != nil {
+				fmt.Printf("failed to cycle to %q: %s\n", err)
+				os.Exit(1)
+			}
+			remaining = *maxSize * 1024 * 1024
+		}
+		n, err := fmt.Fprintln(f, scn.Text())
+		if err != nil {
+			fmt.Printf("failed to write to %q: %s\n", err)
+			os.Exit(1)
+		}
+		remaining -= n
+	}
+	if scn.Err() != nil {
+		fmt.Printf("scanner ended with %q\n", scn.Err())
+	}
+}
+
+func replaceF(filename string, handle *os.File) (*os.File, error) {
+	err := handle.Sync()
+	if err != nil {
+		fmt.Printf("failed to sync %q: %s\n", err)
+	}
+	err = handle.Close()
+	if err != nil {
+		fmt.Printf("error closing %q: %s\n", err)
+	}
+	stamp := time.Now().Format("2006-01-02_150405")
+	ext := filepath.Ext(filename)
+	name := fmt.Sprintf("%s_%s%s", filename[:len(filename)-len(ext)], stamp, ext)
+	err = os.Rename(filename, name)
+	if err != nil {
+		fmt.Printf("error renaming %q: %s\n", err)
+	}
+	return os.Create(filename)
+}
